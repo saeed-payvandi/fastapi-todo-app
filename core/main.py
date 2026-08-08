2c5681ef7997b2cb1b3756636497b53a0d5b0889
saeed-payvandi <saeedpayvandi82@gmail.com>
2026-08-08 07:41:43 +0200

    Set up project structure and add task routes

---
+++ core/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from tasks.routes import router as tasks_routes
+
+tags_metadata = [
+    {
+        "name": "tasks",
+        "description": "Operation related to tasks managment",
+        "externalDocs": {
+            "description": "More about tasks",
+            "url": "https:/example.com/docs/tasks",
+        }
+    }
+]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application startup")
+    yield
+    print("Application shutdown")
+
+app = FastAPI(
+    lifespan=lifespan,
+    openapi_tags=tags_metadata,
+    title="Todo Application",
+    description="A task management application for creating, updating, and tracking tasks",
+    summary="Manage your tasks efficiently",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Example Name",
+        "url": "https://example.com/contact/",
+        "email": "contact@example.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://license.org/mit/",
+    },
+)
+
+# app.include_router(tasks_routes, prefix="/api/v1")
+app.include_router(tasks_routes)
---
+++ core/tasks/routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+# router = APIRouter(tags=["tasks"], prefix="/todo")
+router = APIRouter(tags=["tasks"])
+
+
+@router.get("/tasks")
+async def retrieve_tasks_list():
+    return []
+
+
+@router.get("/tasks/{task_id}")
+async def retrieve_tasks_detail(task_id: int):
+    return []
